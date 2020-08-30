aebe6653afa69c08dd0676aeb79e1c50e371da9b
issertmg <issertmg@gmail.com>
2020-08-30 23:11:25 +0800

    created GUI for server

--- Server.java
+++ Server.java
@@ -1,15 +1,49 @@
 import java.io.*; 
 import java.text.*; 
 import java.util.*; 
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.event.*;
 import java.net.*; 
 
 public class Server {
 	int nPort;
 	Set<ClientHandler> clientHandlers;
+	JButton saveBtn;
+	JTable table;
+	JScrollBar scrollBar;
+	JFrame frame;
+	DefaultTableModel model;
 
 	public Server(int nPort) {
 		this.nPort = nPort;
-		clientHandlers = new HashSet<>();	
+		clientHandlers = new HashSet<>();
+		init();	
+	}
+
+	public void init () {
+		frame = new JFrame();
+		frame.setResizable(false);
+		saveBtn = new JButton("save log");
+		//sendBtn.setSize(150, 80);
+		
+		frame.add(saveBtn, BorderLayout.SOUTH);
+
+		String column[] = {"Timestamp", "Source", "Destination", "Activity"};
+		model = new DefaultTableModel(column, 0);
+		table = new JTable(model);
+
+		// Object[] row = {"data1", "data2" , "data3", "data4"};
+		// model.addRow( row );
+		JScrollPane scrollpane = new JScrollPane(table);
+		scrollBar = scrollpane.getVerticalScrollBar();
+		frame.add(scrollpane, BorderLayout.CENTER);
+		
+		frame.setSize(400, 600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//addActionListeners();
 	}
 
 	public void execute () {
